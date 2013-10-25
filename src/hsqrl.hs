28cd76e4fe3c3c96faefc9b9054ddc57d123bb78
Mark Schultz <mxs802@case.edu>
2013-10-25 11:35:12 -0500

    start getting random bits

--- src/hsqrl.hs
+++ src/hsqrl.hs
@@ -1,18 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Crypto.Scrypt
---import Crypto.Random
---import Data.Digest.Pure.SHA
-import Data.ByteString as B
-import Data.Maybe
+--import Crypto.Scrypt
+import Crypto.Random
+--import Data.Maybe
+import Data.ByteString
 
 main :: IO ()
 main = error "error"
 
-genMasterKey :: Integer -> Integer -> Integer -> ByteString -> ByteString -> ByteString
-genMasterKey logN r p sa pa = unHash $ scrypt params salt pass
+genMasterKey' sz = fst $ cprgGenerate sz cprg
     where
-        params = fromJust $ scryptParams logN r p
-        pass = Pass pa
-        salt = Salt sa
+        cprg = createEntropyPool >>= \pool -> cprgCreate pool
