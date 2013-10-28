3d185ce8d3d4069bd63fe7fdf9567d1429423f75
Mark Schultz <mxs802@case.edu>
2013-10-28 16:48:54 -0500

    generate private key and convert to bitarray

--- src/hsqrl.hs
+++ src/hsqrl.hs
@@ -4,11 +4,22 @@ module Main where
 --import Crypto.Scrypt
 import Crypto.Random
 --import Data.Maybe
-import Data.ByteString
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.ByteString as BS
+import Control.Monad
+import qualified Data.Binary.Strict.BitGet as BG
+import Data.Either.Unwrap
 
 main :: IO ()
 main = error "error"
 
-genMasterKey' sz = fst $ cprgGenerate sz cprg
-    where
-        cprg = createEntropyPool >>= \pool -> cprgCreate pool
+genMasterKey :: Int -> BS.ByteString
+--genMasterKey sz = do
+genMasterKey sz = unsafePerformIO $ do
+        pool <- createEntropyPool
+        let cprg = cprgCreate pool :: SystemRNG
+        let (b,_) = cprgGenerate sz cprg
+        return b
+
+bsToBl :: Int -> BS.ByteString -> [Bool]
+bsToBl n bs = fromRight $ BG.runBitGet bs (replicateM n BG.getBit)
