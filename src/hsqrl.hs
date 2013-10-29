324c443a6035cd686dc087acff23971492e8855c
Mark Schultz <mxs802@case.edu>
2013-10-29 16:55:49 -0500

    add function to convert base and fold to do the same

--- src/hsqrl.hs
+++ src/hsqrl.hs
@@ -7,17 +7,23 @@ import Crypto.Random
 import qualified Data.ByteString as BS
 import Control.Monad
 import qualified Data.Binary.Strict.BitGet as BG
-import Data.Binary as B
 import Data.Either.Unwrap
+import Data.Digits
 
 main :: IO ()
 main = do
         mk <- genMasterKey 66
         putStrLn $ "key is " ++ show (BS.length mk) ++ " bytes long."
-        let bits = bsToBl 521 mk
-        --putStrLn $ show $ length bits
-        --putStrLn $ show $ BS.length $ trimLeft 7 mk
-        putStrLn $ show $ bsToBl 528 $ trimLeft 1 mk
+        --putStrLn $ show $ B6.encode mk
+        --putStrLn $ show $ B6.encode $ trimLeft 7 mk
+        let bools = bsToBl (BS.length mk * 8) mk
+        --putStrLn $ show $ take 8 bools
+        putStrLn $ show $ btoi $ take 8 bools
+        putStrLn $ show $ foldli (\z acc x -> z + (2 ^ acc)*x) 0 (8-1) (btoi $ take 8 bools)
+        putStrLn "--------------"
+        putStrLn $ show $ btoi $ bools
+        putStrLn $ show $ foldli (\z acc x -> z + (2 ^ acc)*x) 0 (length bools - 1) (btoi $ bools)
+        putStrLn $ show $ convertBase 2 10 (btoi $ bools)
 
 genMasterKey :: Int -> IO BS.ByteString
 genMasterKey sz = do
@@ -32,3 +38,15 @@ bsToBl n bs = fromRight $ BG.runBitGet bs (replicateM n BG.getBit)
 -- this pads left with false
 trimLeft :: Int -> BS.ByteString -> BS.ByteString
 trimLeft pad bs = fromRight $ BG.runBitGet bs (BG.getRightByteString ((BS.length bs * 8) - pad) )
+
+--btoi' (x:xs) ni sum = sum + ((2 ^ ni) * (if x == True then 1 else 0)) 
+btoi :: [Bool] -> [Integer]
+btoi [] = []
+btoi (x:xs) = (if x == True then [1] :: [Integer] else [0] :: [Integer]) ++ btoi xs
+
+foldli :: Num a => (t -> a -> t1 -> t) -> t -> a -> [t1] -> t
+foldli f z acc (x:xs) = foldli f (f z acc x) (acc-1) xs
+foldli _ z _ [] = z
+
+convertBase :: Integral a => a -> a -> [a] -> [a]
+convertBase from to = digits to . unDigits from
