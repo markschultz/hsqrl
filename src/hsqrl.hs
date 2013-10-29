9cb57f75491a176e52c566cc5e0f3129607231d0
Mark Schultz <mxs802@case.edu>
2013-10-28 23:03:24 -0500

    remove unsafePerformIO

--- src/hsqrl.hs
+++ src/hsqrl.hs
@@ -4,18 +4,22 @@ module Main where
 --import Crypto.Scrypt
 import Crypto.Random
 --import Data.Maybe
-import System.IO.Unsafe (unsafePerformIO)
+--import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.ByteString as BS
 import Control.Monad
 import qualified Data.Binary.Strict.BitGet as BG
 import Data.Either.Unwrap
 
 main :: IO ()
-main = error "error"
+main = do
+        mk <- genMasterKey 66
+        putStrLn $ "key is " ++ show (BS.length mk) ++ " bytes long."
+        let bits = bsToBl 521 mk
+        putStrLn $ show $ length bits
 
-genMasterKey :: Int -> BS.ByteString
+genMasterKey :: Int -> IO BS.ByteString
 --genMasterKey sz = do
-genMasterKey sz = unsafePerformIO $ do
+genMasterKey sz = do
         pool <- createEntropyPool
         let cprg = cprgCreate pool :: SystemRNG
         let (b,_) = cprgGenerate sz cprg
