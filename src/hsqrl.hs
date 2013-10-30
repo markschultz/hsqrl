056178c146ebd3ea746d683bf41cc865abd80202
Mark Schultz <mxs802@case.edu>
2013-10-30 16:44:17 -0500

    start generating the correct length key

--- src/hsqrl.hs
+++ src/hsqrl.hs
@@ -10,19 +10,29 @@ import qualified Data.Binary.Strict.BitGet as BG
 import Data.Either.Unwrap
 import Data.Digits
 
+n521 :: Integer
+n521 = 6864797660130609714981900799081393217269435300143305409394463459185543183397655394245057746333217197532963996371363321113864768612440380340372808892707005449
+
 main :: IO ()
 main = do
-        mk <- genMasterKey 66
+        let keyLen = length $ digits 2 n521
+        let pad = 8 - (keyLen `mod` 8)
+        --let genLen = ((keyLen + 64) / 8) :: Int
+        mk <- genMasterKey ((keyLen + pad + 64) `div` 8)
         putStrLn $ "key is " ++ show (BS.length mk) ++ " bytes long."
         --putStrLn $ show $ B6.encode $ trimLeft 7 mk
         let bools = bsToBl (BS.length mk * 8) mk
-        let boolsT = drop 7 $ bsToBl ((BS.length mk * 8) ) $ trimLeft 7 mk
-        putStrLn $ show $ btoi $ bools
-        putStrLn $ show $ btoi $ boolsT
-        putStrLn "-------foldli-------"
-        putStrLn $ show $ foldli (\z acc x -> z + (2 ^ acc)*x) 0 (length bools - 1) (btoi $ bools)
-        putStrLn "-------convert------"
-        putStrLn $ show $ foldl (\a b -> b + (10*a)) (0 :: Integer) (convertBase 2 10 (btoi $ bools))
+        putStrLn $ show pad ++ " = pad length"
+        --let bools2 = drop 13 $ bsToBl ((BS.length mk * 8) ) $ trimLeft 7 mk
+        --let bools3 = drop 7 $ bsToBl ((BS.length mk * 8) ) mk
+        --putStrLn $ show $ btoi $ bools
+        --putStrLn $ show $ btoi $ bools2
+        --putStrLn $ show $ btoi $ bools3
+        --putStrLn "-------foldli-------"
+        --putStrLn $ show $ foldli (\z acc x -> z + (2 ^ acc)*x) 0 (length bools - 1) (btoi $ bools)
+        --putStrLn "-------convert------"
+        --putStrLn $ show $ foldl (\a b -> b + (10*a)) (0 :: Integer) (convertBase 2 10 (btoi $ bools))
+        
 
 genMasterKey :: Int -> IO BS.ByteString
 genMasterKey sz = do
@@ -33,10 +43,6 @@ genMasterKey sz = do
 
 bsToBl :: Int -> BS.ByteString -> [Bool]
 bsToBl n bs = fromRight $ BG.runBitGet bs (replicateM n BG.getBit)
-
--- this pads left with false
-trimLeft :: Int -> BS.ByteString -> BS.ByteString
-trimLeft pad bs = fromRight $ BG.runBitGet bs (BG.getLeftByteString ((BS.length bs * 8) - pad) )
 
 btoi :: [Bool] -> [Integer]
 btoi [] = []
